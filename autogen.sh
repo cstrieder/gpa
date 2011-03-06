f15582a66fbd1219fe82b130473bcfb8f101748b
cstrieder <cstrieder@35a2e611-1ee3-4be0-80c5-6d6395bcf02a>
2011-03-06 04:26:38 +0000

    more files to make all work

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,10 @@
 
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
-touch NEWS README AUTHORS ChangeLog
+echo "Cristiano Strieder cstrieder@gmail.com" > AUTHORS
+echo "Reinaldo R. Rosa reinaldo@lac.inpe.br" >> AUTHORS
+echo `date` > README
+echo "This is the first release available at svn" > ChangeLog
+touch NEWS
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
