d595a8c988d436ea2fc4b86360ecc43f7dfbabba
cstrieder <cstrieder@35a2e611-1ee3-4be0-80c5-6d6395bcf02a>
2011-03-09 22:02:50 +0000

    test

---
+++ gpa/src/gpa.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gpa.cpp
+// Author      : Cristiano Strieder
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
