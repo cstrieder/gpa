71b02c90b94f9965ab9361c8c3ae45fd1d3a3aca
cstrieder <cstrieder@gmail.com>
2011-03-09 23:31:15 +0000

    Added info for developers.

--- autogen.sh
+++ autogen.sh
@@ -5,11 +5,16 @@ test -n "$srcdir" || srcdir=.
 echo "Cristiano Strieder cstrieder@gmail.com" > AUTHORS
 echo "Reinaldo R. Rosa reinaldo@lac.inpe.br" >> AUTHORS
 echo `date` > NEWS
-echo "Follow this instructions to install:" > README
+echo "To install:" > README
 echo "" >> README
 echo "./configure" >> README
 echo "make" >> README
 echo "./gpa-install.sh" >> README
+echo "" >> README
+echo "To make a new release:" >> README
+echo "" >> README
+echo "./clean.sh" >> README
+echo "./autogen.sh" >> README
 echo "This is the first release available at svn" > ChangeLog
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
